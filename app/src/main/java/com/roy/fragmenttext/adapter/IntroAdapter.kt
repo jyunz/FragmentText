7d2358dcf6019ae96428e476be1d071325c82157
Roy <J.jyun0525@gmail.com>
2022-08-19 15:45:40 +0900

    intro adapter에 로고이미지 빼고 작성 -> 이후 프래그먼트에 연결

--- app/src/main/java/com/roy/fragmenttext/adapter/IntroAdapter.kt
+++ app/src/main/java/com/roy/fragmenttext/adapter/IntroAdapter.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
 import com.roy.fragmenttext.R
 import com.roy.fragmenttext.data.MemberData
 
@@ -18,12 +20,18 @@ class IntroAdapter(
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         var tempRow = convertView
         if (tempRow == null) {
-            tempRow = inflater.inflate(R.layout., null)
+            tempRow = inflater.inflate(R.layout.intro_list_item, null)
         }
 
         val row = tempRow!!
 
         val data = mList[position]
+
+        val memberNameTxt = row.findViewById<TextView>(R.id.memberName)
+
+        memberNameTxt.text = data.name
+
+        return row
 
     }
 }
