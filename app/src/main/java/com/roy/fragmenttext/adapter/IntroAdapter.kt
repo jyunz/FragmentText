ab8d1081bada397ef2c9a0499360d41f714b0eef
Roy <J.jyun0525@gmail.com>
2022-08-19 15:39:22 +0900

    intro list item 작성

--- app/src/main/java/com/roy/fragmenttext/adapter/IntroAdapter.kt
+++ app/src/main/java/com/roy/fragmenttext/adapter/IntroAdapter.kt
@@ -1,4 +1,29 @@
 package com.roy.fragmenttext.adapter
 
-class IntroAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.roy.fragmenttext.R
+import com.roy.fragmenttext.data.MemberData
+
+class IntroAdapter(
+    val mContext : Context,
+    val resId : Int,
+    val mList : List<MemberData>) : ArrayAdapter<MemberData>(mContext, resId, mList){
+
+    val inflater = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+        if (tempRow == null) {
+            tempRow = inflater.inflate(R.layout., null)
+        }
+
+        val row = tempRow!!
+
+        val data = mList[position]
+
+    }
 }
